3d915b0d805570b1b24a2c71b5cb748b41416e37
Ritik63988 <ritiktyagi783@gmail.com>
2026-07-15 10:35:32 +0530

    fifth

--- obj/Debug/net10.0/MyWebApiApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyWebApiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558582f8691f2bb41e1ad3e51afa84dbf151646e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fd8f949bcff5eefc5c167c6e9c3a28f6e3c452")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
